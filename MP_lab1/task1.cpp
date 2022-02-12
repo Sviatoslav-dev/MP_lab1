fb6704b86cc66312623c30e73d43f7c565381ad7
Sviatoslav <slaviktkachuk11@gmail.com>
2022-02-12 20:22:21 +0200

    done task2

--- MP_lab1/task1.cpp
+++ MP_lab1/task1.cpp
@@ -4,141 +4,38 @@
 
 using namespace std;
 
-//int i = 0;
-//start_for:
-//if (i < N) {
-//
-//
-//    i++;
-//    goto start_for;
-//}
-
-//start_while:
-//if (true) {
-//
-//    goto start_while;
-//}
-
-/*
-int main()
-{
-    int N = 10, file_size;
-    char* chars;
-    char** words;
-    ifstream file("some_text.txt");
-    file.seekg(0, std::ios::end);
-    file_size = file.tellg();
-    file.seekg(0, std::ios::beg);
-    cout << file_size << endl;
-    chars = new char[file_size];
-
-    int max_words = file_size / 4;
-    words = new char* [max_words];
-
-    int i1 = 0;
-    start_for1:
-    if (i1 < max_words) {
-        words[i1] = new char[file_size];
-
-        int j1 = 0;
-        start_for2:
-        if (j1 < file_size) {
-            words[i1][j1] = '$';
-            j1++;
-            goto start_for2;
-        }
-
-        i1++;
-        goto start_for1;
-    }
-
-    file.read(chars, file_size);
-
-    int i2 = 0, word_i = 0, word_char_i = 0;
-    bool is_word = false;
-    start_for3:
-    if (i2 < file_size) {
-        if ((chars[i2] >= 'a' && chars[i2] <= 'z') || (chars[i2] >= 'A' && chars[i2] <= 'Z') || chars[i2] == '\'') {
-            is_word = true;
-        }
-        else {
-            if (is_word) {
-                word_i++;
-                word_char_i = 0;
-                is_word = false;
-            }
-        }
-
-        if (is_word) {
-            if (chars[i2] >= 'A' && chars[i2] <= 'Z') {
-                words[word_i][word_char_i] = chars[i2]  - 'A' + 'a';
-            }
-            else {
-                words[word_i][word_char_i] = chars[i2];
-            }
-            word_char_i++;
-        }
-    
-        i2++;
-        goto start_for3;
-    }
-
-    int i3 = 0;
-    start_for4:
-    if (i3 < max_words) {
-        cout << words[i3][0] << words[i3][1] << words[i3][2] << words[i3][3] << words[i3][4] << words[i3][5] << words[i3][6] << words[i3][7] << endl;
-    
-        i3++;
-        goto start_for4;
-    }
-    
-    int i = 0;
-    start_for:
-    if (i < file_size) {
-        cout << chars[i];
-        i++;
-        goto start_for;
-    }
-
-    delete[] chars;
-    file.close();
-}*/
-
 int main() {
-    int N = 10, file_size;
+    int N = 25;
     ifstream file("some_text.txt");
     string text = "";
-
-    file.seekg(0, std::ios::end);
-    file_size = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    int max_words = file_size / 4;
-    string* words = new string[max_words];
-    string* unique_words = new string[max_words];
-    int* word_count = new int[max_words];
-
-    int i = 0;
-    start_for:
-    if (i < max_words) {
-        word_count[i] = 0;
     
-        i++;
-        goto start_for;
-    }
-
-    start_while:
+    string s;
+    read_file:
     if (!file.eof()) {
-        string s = "";
+        s = "";
         getline(file, s);
         text += s + '\n';
-        goto start_while;
+        goto read_file;
+    }
+
+    int max_words = text.size() / 2;
+    string* words = new string[max_words];
+    string* unique_words = new string[max_words];
+    int* words_count = new int[max_words];
+
+    int i = 0;
+    initilize_word_count:
+    if (i < max_words) {
+        words_count[i] = 0;
+
+        i++;
+        goto initilize_word_count;
     }
 
     int i1 = 0, word_i = 0;
     bool is_word = false;
-    start_for1:
-    if (i1 < text.size()) {
+    text_traversal:
+    if (text[i1] != '\0') {
         if ((text[i1] >= 'a' && text[i1] <= 'z') || (text[i1] >= 'A' && text[i1] <= 'Z') || text[i1] == '\'') {
             is_word = true;
         }
@@ -159,82 +56,66 @@ int main() {
         }
 
         i1++;
-        goto start_for1;
+        goto text_traversal;
     }
 
     int i3 = 0;
-    start_while1:
+    words_traversal:
     if (words[i3] != "") {
         int i4 = 0;
         if (words[i3].size() < 4) {
-            goto word_exist;
+            goto miss_word;
         }
 
-        start_while2:
+        unique_words_traversal:
         if (unique_words[i4] != "") {
             if (words[i3] == unique_words[i4]) {
-                word_count[i4]++;
-                goto word_exist;
+                words_count[i4]++;
+                goto miss_word;
             }
 
             i4++;
-            goto start_while2;
+            goto unique_words_traversal;
         }
         else {
             unique_words[i4] = words[i3];
-            word_count[i4]++;
+            words_count[i4]++;
         }
 
-        word_exist:
+        miss_word:
 
         i3++;
-        goto start_while1;
+        goto words_traversal;
     }
 
-    /*int i2 = 0;
-    start_for2:
-    if (i2 < max_words) {
-        cout << words[i2] << endl;
-
-        i2++;
-        goto start_for2;
-    }*/
-
     int i6 = 0;
-    start_for4:
+    sort:
     if (i6 < N) {
         string max_unique_word;
         int max_word_count = 0;
         int max_i7 = 0;
 
         int i7 = 0;
-        start_while5:
+        step:
         if (unique_words[i7] != "") {
-            if (word_count[i7] > max_word_count) {
+            if (words_count[i7] > max_word_count) {
                 max_unique_word = unique_words[i7];
-                max_word_count = word_count[i7];
+                max_word_count = words_count[i7];
                 max_i7 = i7;
             }
             i7++;
-            goto start_while5;
+            goto step;
         }
 
-        word_count[max_i7] = -word_count[max_i7];
+        words_count[max_i7] = -words_count[max_i7];
         cout << max_unique_word << "   " << max_word_count << endl;
     
         i6++;
-        goto start_for4;
+        goto sort;
     }
 
-    /*int i5 = 0;
-    start_while3:
-    if (unique_words[i5] != "") {
-        cout << unique_words[i5] << "   " << word_count[i5] << endl;
-        i5++;
-        goto start_while3;
-    }*/
-
-    //cout << text << endl;
-
+    delete[] words;
+    delete[] unique_words;
+    delete[] words_count;
     file.close();
 }
---
+++ Task2/Task2.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int page_size = 45;
+    ifstream file("some_book.txt");
+    string text = "";
+
+    string s;
+    read_file:
+    if (!file.eof()) {
+        s = "";
+        getline(file, s);
+        text += s + '\n';
+        goto read_file;
+    }
+
+    int max_words = text.size();
+    string* words = new string[max_words];
+    string* unique_words = new string[max_words];
+
+    int* words_pages = new int[max_words];
+
+    int i = 0;
+    initialize_words_pages:
+    if (i < max_words) {
+        words_pages[i] = 0;
+        i++;
+        goto initialize_words_pages;
+    }
+
+    int* words_count = new int[max_words];
+
+    int i2 = 0;
+    initialize_words_count:
+    if (i2 < max_words) {
+        words_count[i2] = 0;
+
+        i2++;
+        goto initialize_words_count;
+    }
+
+    int line = 0;
+    int i1 = 0, word_i = 0;
+    bool is_word = false;
+    text_traversal:
+    if (i1 < text.size()) {
+        if ((text[i1] >= 'a' && text[i1] <= 'z') || (text[i1] >= 'A' && text[i1] <= 'Z') || text[i1] == '\'') {
+            is_word = true;
+        }
+        else {
+            if (is_word) {
+                words_pages[word_i] = line / page_size + 1;
+                word_i++;
+                is_word = false;
+            }
+        }
+
+        if (text[i1] == '\n') {
+            line++;
+        }
+
+        if (is_word) {
+            if (text[i1] >= 'A' && text[i1] <= 'Z') {
+                words[word_i] += text[i1] - 'A' + 'a';
+            }
+            else {
+                words[word_i] += text[i1];
+            }
+        }
+
+        i1++;
+        goto text_traversal;
+    }
+
+
+    int i3 = 0;
+    words_traversal:
+    if (words[i3] != "") {
+        int i4 = 0;
+
+        unique_words_traversal:
+        if (unique_words[i4] != "") {
+            if (words[i3] == unique_words[i4]) {
+                words_count[i4]++;
+                goto miss_word;
+            }
+
+            i4++;
+            goto unique_words_traversal;
+        }
+        else {
+            unique_words[i4] = words[i3];
+            words_count[i4]++;
+        }
+
+        miss_word:
+
+        i3++;
+        goto words_traversal;
+    }
+
+    bool was_replased = true;
+    string temp_word = "";
+    int temp_count = 0;
+    sort:
+    if (was_replased) {
+        was_replased = false;
+        int i5 = 0;
+        step:
+        if (words_count[i5 + 1] > 0 && i5 + 1 < max_words) {
+            if (unique_words[i5] > unique_words[i5 + 1]) {
+                temp_word = unique_words[i5];
+                unique_words[i5] = unique_words[i5 + 1];
+                unique_words[i5 + 1] = temp_word;
+
+                temp_count = words_count[i5];
+                words_count[i5] = words_count[i5 + 1];
+                words_count[i5 + 1] = temp_count;
+                was_replased = true;
+            }
+
+            i5++;
+            goto step;
+        }
+        goto sort;
+    }
+
+    int i6 = 0;
+    print_words_and_pages:
+    if (i6 < 20) {
+        if (words_count[i6] < 100) { 
+            cout << unique_words[i6] << " - ";
+            int i7 = 0;
+            print_pages:
+            if (words[i7] != "") {
+                
+                if (words[i7] == unique_words[i6]) {
+                    cout << words_pages[i7] << ' ';
+                }
+
+                i7++;
+                goto print_pages;
+            }
+            cout << endl;
+        }
+
+        i6++;
+        goto print_words_and_pages;
+    }
+
+    file.close();
+}
+
